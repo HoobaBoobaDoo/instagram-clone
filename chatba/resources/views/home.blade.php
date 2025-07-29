b808ed4597e0473d61763ebfbed7d248e7a3258d
Hooba <ruben.jamart@gmail.com>
2025-07-29 14:09:08 +0200

    auth done :)

--- chatba/resources/views/home.blade.php
+++ chatba/resources/views/home.blade.php
@@ -6,6 +6,35 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Test 123</h1>
+
+    <div style="border: 3px solid black; padding: 20px; margin: 20px;">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is a simple home page for our application.</p>
+    </div>
+    @auth
+    <p>Welcome back, {{ auth()->user()->name }}!</p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+    @else
+        <h2>Register:</h2>
+        <form action="/register" method="POST">
+            @csrf
+        <input name="name" type="text" placeholder="Username" style="display: block; margin-bottom: 10px;">
+        <input name="email" type="email" placeholder="Email" style="display: block; margin-bottom: 10px;">
+        <input name="password" type="password" placeholder="Password" style="display: block; margin-bottom: 10px;">
+        <button>Register</button>
+        </form>
+        <h2>Login:</h2>
+        <form action="/login" method="POST">
+            @csrf
+        <input name="loginname" type="text" placeholder="Username" style="display: block; margin-bottom: 10px;">
+        <input name="loginpassword" type="password" placeholder="Password" style="display: block; margin-bottom: 10px;">
+        <button>Login</button>
+        </form>
+    @endauth
+
+    
 </body>
 </html>
--- chatba/routes/web.php
+++ chatba/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::post('/register', UserController::class . '@register')->name('register');
+
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::post('/login', [UserController::class, 'login'])->name('login');
